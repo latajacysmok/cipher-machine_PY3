d6fd3b01d3493a06fa24cf857e3fafa1c8faf0e6
Maks <maksymles@gmail.com>
2018-02-15 15:22:56 +0100

    add coments

--- encryption.py
+++ encryption.py
@@ -1,13 +1,15 @@
+#a cipher is given which changes the vowels according to the key:
+#key = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
+#The program has a cryptographic function and one that can decrypt the text.
+
 def encryption(s):
-    klucz = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
-    answer = [klucz[letter] if letter in klucz.keys() else letter for letter in s]
+    key = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
+    answer = key[letter] if letter in key.keys() else letter for letter in s]
     print("".join(answer))
 
-encryption("to jest moj tekst")
 
 def decrypt(w):
-    klucz = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
-    answer = [list(klucz.keys())[list(klucz.values()).index(letter)] if letter in list(klucz.values()) else letter for letter in w]
+    key = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
+    answer = [list(key.keys())[list(key.values()).index(letter)] if letter in list(key.values()) 
+		else letter for letter in w]
     print("".join(answer))
-
-decrypt("noi zypomnoj a dakumintycjo")
